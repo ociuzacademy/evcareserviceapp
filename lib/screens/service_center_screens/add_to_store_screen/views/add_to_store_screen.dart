e174a93749a1db210d981345f8c7ec8f83264405
MikhilMC <mikhilmohan13@gmail.com>
2025-01-20 12:26:16 +0530

    feat[API integration]: API integration for adding new product to the store

--- lib/screens/service_center_screens/add_to_store_screen/views/add_to_store_screen.dart
+++ lib/screens/service_center_screens/add_to_store_screen/views/add_to_store_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:evcareserviceapp/common_widgets/form_text_field_without_icon.dart';
 import 'package:evcareserviceapp/common_widgets/padded_elevated_button.dart';
+import 'package:evcareserviceapp/screens/service_center_screens/add_to_store_screen/services/add_product.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -21,8 +22,9 @@ class _AddToStoreScreenState extends State<AddToStoreScreen> {
   final TextEditingController _productQuantityController =
       TextEditingController();
 
-  bool isImageSelected = false;
-  File? imageFile;
+  bool _isImageSelected = false;
+  File? _imageFile;
+  bool _isAddingProduct = false;
 
   @override
   void dispose() {
@@ -40,13 +42,32 @@ class _AddToStoreScreenState extends State<AddToStoreScreen> {
       builder: (context) {
         return AlertDialog(
           title: const Text("Error"),
-          content: Text(message),
+          backgroundColor: Colors.black,
+          surfaceTintColor: Colors.green.shade100,
+          titleTextStyle: const TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          content: Text(
+            message,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 15,
+            ),
+          ),
           actions: [
             TextButton(
               onPressed: () {
                 Navigator.of(context).pop();
               },
-              child: const Text("OK"),
+              child: const Text(
+                "OK",
+                style: TextStyle(
+                  color: Colors.greenAccent,
+                  fontSize: 15,
+                ),
+              ),
             )
           ],
         );
@@ -60,8 +81,8 @@ class _AddToStoreScreenState extends State<AddToStoreScreen> {
           await ImagePicker().pickImage(source: ImageSource.gallery);
       if (pickedImage != null) {
         setState(() {
-          imageFile = File(pickedImage.path);
-          isImageSelected = true;
+          _imageFile = File(pickedImage.path);
+          _isImageSelected = true;
         });
       }
     } catch (e) {
@@ -77,13 +98,52 @@ class _AddToStoreScreenState extends State<AddToStoreScreen> {
 
       if (pickedImage != null) {
         setState(() {
-          imageFile = File(pickedImage.path);
-          isImageSelected = true;
+          _imageFile = File(pickedImage.path);
+          _isImageSelected = true;
         });
       }
     } catch (e) {
       // print('Error: ${e.toString()}');
       _showErrorDialogue('Error: ${e.toString()}');
+    }
+  }
+
+  Future<void> _addProduct() async {
+    // Validate will return true if the form is valid, or false if
+    // the form is invalid.
+    if (_formKey.currentState!.validate() && _imageFile != null) {
+      setState(() {
+        _isAddingProduct = true;
+      });
+      try {
+        final response = await addProduct(
+          productName: _productNameController.text,
+          productDescription: _productDescriptionController.text,
+          productPrice: _productPriceController.text,
+          productQuantity: _productQuantityController.text,
+          productImage: _imageFile!,
+          serviceCenterId: "2",
+        );
+
+        bool status = response.status == "success";
+
+        if (status && mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text("Adding new product success")),
+          );
+          Navigator.of(context).pop();
+        }
+      } catch (e) {
+        _showErrorDialogue(e.toString());
+      } finally {
+        setState(() {
+          _isAddingProduct = false;
+        });
+      }
+    } else {
+      _showErrorDialogue(
+        "Please enter all the fields and select an image from either camera or gallery",
+      );
     }
   }
 
@@ -105,126 +165,129 @@ class _AddToStoreScreenState extends State<AddToStoreScreen> {
         ),
       ),
       backgroundColor: Colors.black,
-      body: SingleChildScrollView(
-        child: Center(
-          child: SizedBox(
-            height: screenSize.height - 100,
-            width: screenSize.width - 50,
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: screenSize.height / 50,
-                  ),
-                  FormTextFieldWithoutIcon(
-                    hintText: 'Enter product name',
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter product name';
-                      }
-
-                      return null;
-                    },
-                    textEditingController: _productNameController,
-                  ),
-                  SizedBox(
-                    height: screenSize.height / 50,
-                  ),
-                  FormTextFieldWithoutIcon(
-                    hintText: 'Enter product description',
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter product description';
-                      }
-
-                      return null;
-                    },
-                    textEditingController: _productDescriptionController,
-                  ),
-                  SizedBox(
-                    height: screenSize.height / 50,
-                  ),
-                  FormTextFieldWithoutIcon(
-                    hintText: 'Enter product price',
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter product price';
-                      }
-
-                      return null;
-                    },
-                    textEditingController: _productPriceController,
-                  ),
-                  SizedBox(
-                    height: screenSize.height / 50,
-                  ),
-                  FormTextFieldWithoutIcon(
-                    hintText: 'Enter product quantity',
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter product quantity';
-                      }
-
-                      return null;
-                    },
-                    textEditingController: _productQuantityController,
-                  ),
-                  SizedBox(
-                    height: screenSize.height / 50,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      IconButton(
-                        style: const ButtonStyle(
-                          backgroundColor: WidgetStatePropertyAll(
-                            Colors.green,
-                          ),
+      body: _isAddingProduct
+          ? const Center(
+              child: CircularProgressIndicator(
+                color: Colors.green,
+              ),
+            )
+          : SingleChildScrollView(
+              child: Center(
+                child: SizedBox(
+                  height: screenSize.height - 100,
+                  width: screenSize.width - 50,
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: screenSize.height / 50,
                         ),
-                        onPressed: _pickImageFromGallary,
-                        icon: const Icon(
-                          Icons.add_photo_alternate,
-                          color: Colors.white,
+                        FormTextFieldWithoutIcon(
+                          hintText: 'Enter product name',
+                          validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter product name';
+                            }
+
+                            return null;
+                          },
+                          textEditingController: _productNameController,
                         ),
-                      ),
-                      IconButton(
-                        style: const ButtonStyle(
-                          backgroundColor: WidgetStatePropertyAll(
-                            Colors.green,
-                          ),
+                        SizedBox(
+                          height: screenSize.height / 50,
                         ),
-                        onPressed: _pickImageFromCamera,
-                        icon: const Icon(
-                          Icons.add_a_photo,
-                          color: Colors.white,
+                        FormTextFieldWithoutIcon(
+                          hintText: 'Enter product description',
+                          validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter product description';
+                            }
+
+                            return null;
+                          },
+                          textEditingController: _productDescriptionController,
                         ),
-                      ),
-                    ],
-                  ),
-                  PaddedElevatedButton(
-                    buttonText: "Add Product",
-                    onPressed: () {
-                      // Validate will return true if the form is valid, or false if
-                      // the form is invalid.
-                    },
-                  ),
-                  isImageSelected
-                      ? Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Image(
-                              image: FileImage(imageFile!),
+                        SizedBox(
+                          height: screenSize.height / 50,
+                        ),
+                        FormTextFieldWithoutIcon(
+                          hintText: 'Enter product price',
+                          validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter product price';
+                            }
+
+                            return null;
+                          },
+                          textEditingController: _productPriceController,
+                        ),
+                        SizedBox(
+                          height: screenSize.height / 50,
+                        ),
+                        FormTextFieldWithoutIcon(
+                          hintText: 'Enter product quantity',
+                          validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter product quantity';
+                            }
+
+                            return null;
+                          },
+                          textEditingController: _productQuantityController,
+                        ),
+                        SizedBox(
+                          height: screenSize.height / 50,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            IconButton(
+                              style: const ButtonStyle(
+                                backgroundColor: WidgetStatePropertyAll(
+                                  Colors.green,
+                                ),
+                              ),
+                              onPressed: _pickImageFromGallary,
+                              icon: const Icon(
+                                Icons.add_photo_alternate,
+                                color: Colors.white,
+                              ),
                             ),
-                          ),
-                        )
-                      : Container(),
-                ],
+                            IconButton(
+                              style: const ButtonStyle(
+                                backgroundColor: WidgetStatePropertyAll(
+                                  Colors.green,
+                                ),
+                              ),
+                              onPressed: _pickImageFromCamera,
+                              icon: const Icon(
+                                Icons.add_a_photo,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                        PaddedElevatedButton(
+                          buttonText: "Add Product",
+                          onPressed: _addProduct,
+                        ),
+                        _isImageSelected
+                            ? Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Image(
+                                    image: FileImage(_imageFile!),
+                                  ),
+                                ),
+                              )
+                            : Container(),
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-      ),
     );
   }
 }
