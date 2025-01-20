73cab77c9fbb2d23ccdb0e3e4b5f8706252d578f
MikhilMC <mikhilmohan13@gmail.com>
2025-01-20 12:26:16 +0530

    feat[API integration]: API integration for adding new product to the store

---
+++ lib/screens/service_center_screens/add_to_store_screen/services/add_product.dart
@@ -0,0 +1,64 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:evcareserviceapp/common_utils/urls.dart';
+import 'package:http/http.dart' as http;
+import 'package:evcareserviceapp/screens/service_center_screens/add_to_store_screen/models/add_product_response_model.dart';
+
+Future<AddProductResponseModel> addProduct({
+  required String productName,
+  required String productDescription,
+  required String productPrice,
+  required String productQuantity,
+  required File productImage,
+  required String serviceCenterId,
+}) async {
+  try {
+    // Create a multipart request
+    var request = http.MultipartRequest("POST", Uri.parse(Urls.addProductUrl));
+
+    // Add text fields
+    request.fields['name'] = productName;
+    request.fields['description'] = productDescription;
+    request.fields['price'] = productPrice;
+    request.fields['quantity'] = productQuantity;
+    request.fields['service_centre'] = serviceCenterId;
+
+    // Add the image file
+    var imageStream = http.ByteStream(productImage.openRead());
+    var imageLength = await productImage.length();
+    var multiPartFile = http.MultipartFile(
+      'image',
+      imageStream,
+      imageLength,
+      filename: productImage.path.split("/").last,
+    );
+    request.files.add(multiPartFile);
+
+    // Send request
+    final resp = await request.send();
+
+    // Convert the response stream to a string
+    final responseBody = await resp.stream.bytesToString();
+
+    if (resp.statusCode == 200) {
+      final dynamic decoded = jsonDecode(responseBody);
+      final AddProductResponseModel response =
+          AddProductResponseModel.fromJson(decoded);
+      return response;
+    } else {
+      final Map<String, dynamic> errorResponse = jsonDecode(responseBody);
+      throw Exception(
+        'Failed to register: ${errorResponse['message'] ?? 'Unknown error'}',
+      );
+    }
+  } on SocketException {
+    throw Exception('No Internet connection');
+  } on HttpException {
+    throw Exception('Server error');
+  } on FormatException {
+    throw Exception('Bad response format');
+  } catch (e) {
+    throw Exception('Unexpected error: ${e.toString()}');
+  }
+}
