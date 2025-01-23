ff07865590ba69441bf0192c52d82bb56bb76f1f
MikhilMC <mikhilmohan13@gmail.com>
2025-01-23 16:15:40 +0530

    feat[API integration]: API integration for fetching the list of employees in assign employees screen

---
+++ lib/screens/service_center_screens/assign_employee_screen/models/employee_model.dart
@@ -0,0 +1,32 @@
+// To parse this JSON data, do
+//
+//     final employeeModel = employeeModelFromJson(jsonString);
+
+import 'dart:convert';
+
+List<EmployeeModel> employeeModelFromJson(String str) =>
+    List<EmployeeModel>.from(
+        json.decode(str).map((x) => EmployeeModel.fromJson(x)));
+
+String employeeModelToJson(List<EmployeeModel> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class EmployeeModel {
+  final int id;
+  final String name;
+
+  EmployeeModel({
+    required this.id,
+    required this.name,
+  });
+
+  factory EmployeeModel.fromJson(Map<String, dynamic> json) => EmployeeModel(
+        id: json["id"],
+        name: json["name"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+      };
+}
