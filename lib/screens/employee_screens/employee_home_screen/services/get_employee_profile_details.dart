ae81ecd6c909c8c98b1dde1a11b712040b6ae6bc
MikhilMC <mikhilmohan13@gmail.com>
2025-01-24 14:27:41 +0530

    feat[API integration]: API integration for fetching the profile data of an employee

---
+++ lib/screens/employee_screens/employee_home_screen/services/get_employee_profile_details.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:evcareserviceapp/screens/employee_screens/employee_home_screen/models/employee_profile_model.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:evcareserviceapp/common_utils/urls.dart';
+
+Future<EmployeeProfileModel> getEmployeeProfileDetails({
+  required int employeeId,
+}) async {
+  try {
+    Map<String, dynamic> params = {
+      "id": employeeId.toString(),
+    };
+
+    final url = Uri.parse(Urls.getEmplyeeProfileDetailsUrl)
+        .replace(queryParameters: params);
+
+    final resp = await http.get(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    );
+
+    if (resp.statusCode == 200) {
+      final dynamic decoded = jsonDecode(resp.body);
+      final response = EmployeeProfileModel.fromJson(decoded);
+
+      return response;
+    } else {
+      final Map<String, dynamic> errorResponse = jsonDecode(resp.body);
+      throw Exception(
+        '${errorResponse['message'] ?? 'Unknown error'}',
+      );
+    }
+  } on SocketException {
+    throw Exception('Server error');
+  } on HttpException {
+    throw Exception('Something went wrong');
+  } on FormatException {
+    throw Exception('Bad request');
+  } catch (e) {
+    throw Exception(e.toString());
+  }
+}
