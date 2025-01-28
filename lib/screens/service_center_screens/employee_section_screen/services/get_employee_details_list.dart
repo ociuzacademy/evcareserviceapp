650bb34bee78419a877217a18ea09678b4afa561
MikhilMC <mikhilmohan13@gmail.com>
2025-01-28 11:27:20 +0530

    feat[API integration]: API integration to get the detailed list of employees in employee section.

---
+++ lib/screens/service_center_screens/employee_section_screen/services/get_employee_details_list.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:evcareserviceapp/screens/service_center_screens/employee_section_screen/models/employee_model.dart';
+
+import 'package:evcareserviceapp/common_utils/urls.dart';
+
+Future<List<EmployeeModel>> getEmployeeDetailsList({
+  required int serviceCenterId,
+}) async {
+  try {
+    Map<String, dynamic> params = {
+      "service_centre": serviceCenterId.toString(),
+    };
+
+    final url = Uri.parse(Urls.getEmployeesDetailsListUrl)
+        .replace(queryParameters: params);
+
+    final resp = await http.get(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    );
+
+    if (resp.statusCode == 200) {
+      final List<dynamic> decoded = jsonDecode(resp.body);
+      final response =
+          decoded.map((item) => EmployeeModel.fromJson(item)).toList();
+
+      return response;
+    } else {
+      final Map<String, dynamic> errorResponse = jsonDecode(resp.body);
+      throw Exception(
+        '${errorResponse['message'] ?? 'Unknown error'}',
+      );
+    }
+  } on SocketException {
+    throw Exception('Server error');
+  } on HttpException {
+    throw Exception('Something went wrong');
+  } on FormatException {
+    throw Exception('Bad request');
+  } catch (e) {
+    throw Exception(e.toString());
+  }
+}
