42d8a283ab81e9f3f463fcddbbe3040f0809e009
sona <sonaaps250@gmail.com>
2025-03-15 11:21:45 +0530

    fix: change in time, and design
    fix: change in working time, attendance closing time, change in design of employee list widget, purchase item widget

--- lib/common_utils/constants.dart
+++ lib/common_utils/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Constants {
   static const TimeOfDay openingTime = TimeOfDay(
-    hour: 9,
+    hour: 10,
     minute: 0,
   ); // 9:00 AM
   static const TimeOfDay closingTime = TimeOfDay(
@@ -10,7 +10,7 @@ class Constants {
     minute: 0,
   ); // 6:00 PM
   static const TimeOfDay attendanceClosingTime = TimeOfDay(
-    hour: 10,
+    hour: 11,
     minute: 0,
   ); // 10:00 AM
 }
